9c79e4161dafab632f83aff6d379b8812b94cff4
Rose Robertson <rose@getflow.com>
2019-10-26 10:24:43 -0700

    Add 4th demo

---
+++ demos/draft-article-4/precache-manifest.f6ee2dd284f2824f627fe313db13118c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f762a94cbd0c5817c3bff138c305dc08",
+    "url": "/demos/draft-article-4/index.html"
+  },
+  {
+    "revision": "f8b06b673272c387dabf",
+    "url": "/demos/draft-article-4/static/css/main.8dc2a7a3.chunk.css"
+  },
+  {
+    "revision": "ee53561eff38fc0a19dd",
+    "url": "/demos/draft-article-4/static/js/2.365a97da.chunk.js"
+  },
+  {
+    "revision": "f8b06b673272c387dabf",
+    "url": "/demos/draft-article-4/static/js/main.8d0a1df7.chunk.js"
+  },
+  {
+    "revision": "bb0752a8ca2ef058a364",
+    "url": "/demos/draft-article-4/static/js/runtime-main.34a73d9f.js"
+  }
+]);
