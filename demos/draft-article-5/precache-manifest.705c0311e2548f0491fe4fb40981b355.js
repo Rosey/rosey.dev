e9e71edbef27d02c9c2d307c45824f3c59368965
Rose Robertson <rose@getflow.com>
2019-11-01 17:43:24 -0700

    add draft articlae 5

---
+++ demos/draft-article-5/precache-manifest.705c0311e2548f0491fe4fb40981b355.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6c7018cadc02d2ddad818c38e0e971",
+    "url": "/demos/draft-article-5/index.html"
+  },
+  {
+    "revision": "9af03b0138ee869e637a",
+    "url": "/demos/draft-article-5/static/css/main.1ad428d4.chunk.css"
+  },
+  {
+    "revision": "76c6bfe5d219714a8e9a",
+    "url": "/demos/draft-article-5/static/js/2.4f663711.chunk.js"
+  },
+  {
+    "revision": "9af03b0138ee869e637a",
+    "url": "/demos/draft-article-5/static/js/main.38af578d.chunk.js"
+  },
+  {
+    "revision": "29222827a9a3f6f1c95e",
+    "url": "/demos/draft-article-5/static/js/runtime-main.3a079c42.js"
+  }
+]);
